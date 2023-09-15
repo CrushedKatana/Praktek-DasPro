df9e2e95ed2512f13ddfb339e29d850b6ccaf8cb
CrushedKatana <charelsadewo123@gmail.com>
2023-09-15 15:01:11 +0700

    commit

--- Semester 1/Jobsheet1/Latihan07.java
+++ Semester 1/Jobsheet1/Latihan07.java
@@ -4,6 +4,6 @@
 public class Latihan07 {
     public static void main(String[] args) {
         System.out.println("Nama saya Charel");
-        System.out.println(saya adalah mahasiswa baru Jurusan Teknologi Informasi-Polinema);
+        System.out.println("saya adalah mahasiswa baru Jurusan Teknologi Informasi-Polinema');
     }
 }
