8654f33df53bdf21468e80e3040acdd76b0ee306
CrushedKatana <charelsadewo123@gmail.com>
2023-09-15 14:41:22 +0700

    commit

---
+++ Semester 1/Jobsheet 1/Latihan07.java
@@ -0,0 +1,5 @@
+package Praktek-DasPro.Semester 1.Jobsheet 1;
+
+public class Latihan07 {
+    
+}
--- Semester 1/Jobsheet 1/MyFirstJava07.java
+++ Semester 1/Jobsheet 1/MyFirstJava07.java
@@ -1,3 +1,5 @@
+package Praktek-DasPro.Semester 1.Jobsheet 1;
+
 public class MyFirstJava07 {
  public static void main(String[] args) {
     System.out.println("Nama Saya Charellino Kalingga Sadewo");
