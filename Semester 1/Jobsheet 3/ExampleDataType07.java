c0ccaf05bcb2d56beea44ec33d15966514151c11
CrushedKatana <charelsadewo123@gmail.com>
2023-09-15 13:24:48 +0700

    Create ExampleDataType07.java

---
+++ Semester 1/Jobsheet 3/ExampleDataType07.java
@@ -0,0 +1,18 @@
+public class ExampleDataType07 {
+    public static void main(String[] args) {
+        char bloodGroup = 'A';
+        byte distance = (byte) 130;
+        short theNumberOfResidents = 1025;
+        float temperature = 60.50F;
+        double weight = 0.5467812345;
+        long balance = 150000000;
+        int number = 0x10;
+        System.out.println("Blood group\t\t: " + (byte) bloodGroup);
+        System.out.println("Distace\t\t: " + distance);
+        System.out.println("The number od residents\t: + theNumberOfResidents");
+        System.out.println("Temperature \t\t " + temperature );
+        System.out.println("Weigt\t\t\t: " + number);
+        System.out.println("Balance\t\t\t: " + balance);
+        System.out.println();
+       }
+}
