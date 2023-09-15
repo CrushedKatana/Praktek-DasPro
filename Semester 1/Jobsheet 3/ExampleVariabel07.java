93124bddbe40191b9c82826358092a8c2315ae7e
CrushedKatana <charelsadewo123@gmail.com>
2023-09-15 13:06:53 +0700

    commit

--- Semester 1/Jobsheet 3/ExampleVariabel07.java
+++ Semester 1/Jobsheet 3/ExampleVariabel07.java
@@ -9,6 +9,6 @@ System.out.println(oneOfMyHobbies);
 System.out.println("Are you smart ?" + isSmart);
 System.out.println("Gender: "+gender);
 System.out.println("My current age is"+_age);
-
+System.out.println(String.format("My GPA is %s and my height is %s meters", $gpa, height));
 }
 }
