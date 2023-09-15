aafe604024407a5cfb62c9c60ba609300bf4eb6e
CrushedKatana <charelsadewo123@gmail.com>
2023-09-15 12:56:23 +0700

    commit variable

---
+++ Semester 1/Jobsheet 3/ExampleVariabel07.java
@@ -0,0 +1,14 @@
+public class ExampleVariabel07{
+public static void main(String[] args) {
+    String oneOfMyHobbies = "Playing futsal";
+boolean isSmart = true;
+char gender = 'M';
+byte _age = 20;
+double $gpa = 3.38, height = 1.68;
+System.out.println(oneOfMyHobbies);
+System.out.println("Are you smart ?" + isSmart);
+System.out.println("Gender: "+gender);
+System.out.println("My current age is"+_age);
+
+}
+}
