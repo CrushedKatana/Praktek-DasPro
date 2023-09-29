da9584d07cc2aada2224d7268b626aedadd06d16
CrushedKatana <charelsadewo123@gmail.com>
2023-09-29 11:09:55 +0700

    co

--- Semester1/Jobsheet5/Selection1_07.java
+++ Semester1/Jobsheet5/Selection1_07.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class Selection1_07 {
   public static void main(String[] args) {
     Scanner input07 = new Scanner(System.in);
-    System.out.print("Input ");
+    
+    System.out.print("Input a number = ");
+    int number = input07.nextInt();
+
+    if (number%2 == 0){
+      System.out.println(number+" is an even number!");
+    }else{
+      System.out.println(number+" is an odd number!");
+    }
   }  
 }
---
+++ Semester1/Jobsheet5/Selection2_07.java
@@ -0,0 +1,9 @@
+package Semester1.Jobsheet5;
+
+import java.util.Scanner;
+
+public class Selection2_07 {
+    public static void main(String[] args) {
+    Scanner input07
+    }
+}
