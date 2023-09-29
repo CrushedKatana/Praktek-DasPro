10e8905788d52ce3c8eea57acba4305414526464
CrushedKatana <charelsadewo123@gmail.com>
2023-09-29 10:58:01 +0700

    co

---
+++ Semester1/Jobsheet5/Selection1_07.java
@@ -0,0 +1,10 @@
+package Semester1.Jobsheet5;
+
+import java.util.Scanner;
+
+public class Selection1_07 {
+  public static void main(String[] args) {
+    Scanner input07 = new Scanner(System.in);
+    System.out.print("Input ");
+  }  
+}
