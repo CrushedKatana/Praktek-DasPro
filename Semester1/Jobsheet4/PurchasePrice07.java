50b9a57ea8c85e98c3dd48430bc73965b97ef464
CrushedKatana <charelsadewo123@gmail.com>
2023-10-01 18:38:48 +0700

    commit

--- Semester1/Jobsheet4/PurchasePrice07.java
+++ Semester1/Jobsheet4/PurchasePrice07.java
@@ -5,13 +5,21 @@ import java.util.Scanner;
 public class PurchasePrice07 {
     public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    int price, quantity;
-    double discount=0.1, totalPrice, purchasePrice, totalDiscount;
+    int price, quantity, pageCount;
+    double discount, totalPrice, purchasePrice, totalDiscount;
+    String bookBrand;
 
+    
+    System.out.print("Input book brand: ");
+    bookBrand = input.nextLine();
+    System.out.print("Input page count: ");
+    pageCount = input.nextInt();
     System.out.println("Input price: ");
     price=input.nextInt();
     System.out.println("Input quantity: ");
     quantity=input.nextInt();
+    System.out.print("Input discount (in decimal form, e.g., 0.1 for 10%): ");
+    discount = input.nextDouble();
 
     totalPrice=price*quantity;
     totalDiscount=totalPrice*discount;
