de8d8bf47c3b7e49698f5235cb08e64c77b0b3fd
CrushedKatana <charelsadewo123@gmail.com>
2023-10-13 13:09:09 +0700

    Update DoWhileLeaveEntillement07.java

--- Semester1/Jobsheet7/DoWhileLeaveEntillement07.java
+++ Semester1/Jobsheet7/DoWhileLeaveEntillement07.java
@@ -6,10 +6,26 @@ public class DoWhileLeaveEntillement07 {
     public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
-    int leaveEntitlement, numLeave;
-    String confimation;
+    int jatahCuti, jumlahHari;
+    String konfirmasi;
 
+    do{
+        System.out.println("Apakah Anda ingim mengambil cuti (y/t)?");
+        konfirmasi = input.next();
+        
+        if(konfirmasi.equalsIgnoreCase("y")){
+            System.out.println("Jumlah hari: ");
+            jumlahHari = input.nextInt();
+
+            if(jumlahHari <= jatahCuti) {
+                jatahCuti -= jumlahHari;
+                System.out.println("Sisa jatah cuti: "+jatahCuti);
+            } else{
+                System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+                break;
+            }
+        }
+    }while (jatahCuti > 0);
     
-    }
-    
+}  
 }
