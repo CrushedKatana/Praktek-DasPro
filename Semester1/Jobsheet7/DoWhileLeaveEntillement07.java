5396a6f4199b50c9a4451b9aed08eb8634d74e7b
CrushedKatana <charelsadewo123@gmail.com>
2023-10-13 12:56:21 +0700

    Create DoWhileLeaveEntillement07.java

---
+++ Semester1/Jobsheet7/DoWhileLeaveEntillement07.java
@@ -0,0 +1,15 @@
+package Semester1.Jobsheet7;
+
+import java.util.Scanner;
+
+public class DoWhileLeaveEntillement07 {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    int leaveEntitlement, numLeave;
+    String confimation;
+
+    
+    }
+    
+}
