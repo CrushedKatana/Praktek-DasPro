e85ddfbc1ae440b423da074d0bc9f2d6b9f75d56
CrushedKatana <charelsadewo123@gmail.com>
2023-11-02 18:20:30 +0700

    Update ArrayNilai07.java

--- Semester1/Jobsheet9/ArrayNilai07.java
+++ Semester1/Jobsheet9/ArrayNilai07.java
@@ -4,17 +4,15 @@ import java.util.Scanner;
 
 public class ArrayNilai07 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
-            System.out.print("Masukkan nilai akhir ke- " + i + " : ");
-            nilaiAkhir[i] = sc.nextInt();
-
-        } for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir ke - " + i + " : ");
+            nilaiAkhir[i]  = sc.nextInt();
+        }for (int i = 0; i < 10; i++) {
             System.out.println("Nilai akhir ke - " + i + " adalah " + nilaiAkhir[i]);
         }
     }
-    
 }
