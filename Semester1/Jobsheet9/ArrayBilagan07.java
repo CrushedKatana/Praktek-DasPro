0c79abcab5b22c4c189c04f58efae3978ae00c02
CrushedKatana <charelsadewo123@gmail.com>
2023-11-02 17:32:40 +0700

    Update ArrayBilagan07.java

--- Semester1/Jobsheet9/ArrayBilagan07.java
+++ Semester1/Jobsheet9/ArrayBilagan07.java
@@ -3,10 +3,8 @@ package Semester1.Jobsheet9;
 public class ArrayBilagan07 {
     public static void main(String[] args) {
         int[] bil = {5, 13, -7, 17};
-
         
-        
-        for (int i= 0 ; i < 4; i++){
+        for (int i= 0 ; i <= 4; i++){
             System.out.println(bil[i]);
         }
 
