d85d1772ea09a23da3ee10c5ad30146e424e1e39
CrushedKatana <charelsadewo123@gmail.com>
2023-11-06 22:42:44 +0700

    Update Bioskop07.java

--- Semester1/Jobsheet10/Bioskop07.java
+++ Semester1/Jobsheet10/Bioskop07.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Bioskop07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        String [][] penonton = new String[4][2];
+        
+        String[][] penonton = new String[4][2];
 
         penonton[0][0] = "Amin";
         penonton[0][1] = "Bena";
@@ -22,10 +22,11 @@ public class Bioskop07 {
     System.out.printf("%s \t %s\n", penonton[2][0],penonton[2][1]);
     System.out.printf("%s \t %s\n", penonton[3][0],penonton[3][1]);
 
-    System.out.println(penonton.length);
+    for (String[] barisPenonton : penonton) {
+    System.out.println("Panjang baris: " + barisPenonton.length);
+    }
     for (int i = 0; i < penonton.length; i++) {
-    System.out.println("Panjang baris ke-" + (i + 1) + ":" + penonton[i].length);
-
-        }
+        System.out.println("Penonton pada baris ke-" + (i + 1)+ ": " + String.join(", ", penonton[i]));
+    }
     }
 }
