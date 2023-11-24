f341349617b638eb069a7f9214b16f004b9dae0f
CrushedKatana <charelsadewo123@gmail.com>
2023-11-24 11:16:46 +0700

    commit ex 1

---
+++ Semester1/Jobsheet13/Gratitude_07.java
@@ -0,0 +1,11 @@
+package Semester1.Jobsheet13;
+
+public class Gratitude_07 {
+    public static void sayThankyou() {
+        System.out.println("Thank you for being the best teacher in the world.\n"+
+        "you inspired in me a love for learninh and made me feel like i coukd ask you anyting.");
+    }
+    public static void main(String[] args) {
+        sayThankyou();
+    }
+}
