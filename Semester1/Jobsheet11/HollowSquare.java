c58f5caf59a5ec99e56547e619ac6e19cb29f385
CrushedKatana <charelsadewo123@gmail.com>
2023-11-30 11:10:01 +0700

    Update HollowSquare.java

--- Semester1/Jobsheet11/HollowSquare.java
+++ Semester1/Jobsheet11/HollowSquare.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class HollowSquare {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
+        
         System.out.print("Masukan nilai N: ");
-
         int N = sc.nextInt();
         for (int i = 0; i < N; i++){
             System.out.print(N);
